89665e6035fde3c9b63ff961b726d24f220da20e
Erik Wilson <erik@auryntech.com>
2016-07-11 20:00:58 +0000

    resource.rb

---
+++ resources/mongodb.rb
@@ -0,0 +1,14 @@
+resource_name: mongodb
+
+#property :gpgcheck, Boolean, default: true
+#property :gpgkey, String, default: 'https://www.mongodb.org/static/pgp/server-3.2.asc'
+#property :description, String, default: 'MonoDB Repository'
+#property :baseurl, String, default: 'https://repo.mogodb.org/yum/redhat/$releasever/mogodb-org/3.2/x86_64/'
+
+action :create do
+  package 'mongodb-org'
+  service 'mongod' do
+    action [ :enable, :start ]
+  end
+end
+
